5fa4101d68973bd5b5f34f07113b065bd30533f9
zayha <5350061@mail.ru>
2023-10-19 09:26:50 +0300

    l3_solid

---
+++ lesson_03_solid/src/SRP/Emploee.java
@@ -0,0 +1,18 @@
+
+package SRP;
+
+public class Emploee {
+    public String name;
+
+
+    public Emploee(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Emploee{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
